3e88d34ab5997fa7bc05fec821590d7012608d91
Mitesh Sasi Magesh <miteshsasimagesh@Miteshs-MacBook-Air.local>
2026-03-31 22:53:04 +0530

    Removed Basics folder - keeping only Core system

--- Basics/Backendbasics.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
-app.get("/",(req,res)=>{
-    res.send("Airline Disruption Management System Backend running");
-});
-app.listen(PORT,()=>{
-    console.log('Server running on port 3000');
-});
